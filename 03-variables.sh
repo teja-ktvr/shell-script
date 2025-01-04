e10dad89f3b253218599850a463a9ee83132e258
teja-ktvr <itsmektvr@gmail.com>
2025-01-04 18:59:22 +0530

    added conversation

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Rahim
+PERSON2=Robert
+
+echo "$PERSON1:: Hi $PERSON2"
+echo "$PERSON2:: Hello $PERSON1"
+echo "$PERSON1:: How are you"
+echo "$PERSON2:: I am good. How are you?"
