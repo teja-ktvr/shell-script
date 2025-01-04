f446e74e6ffd00e04abc0a2e5baae0193b89912f
teja-ktvr <itsmektvr@gmail.com>
2025-01-04 22:06:12 +0530

    added conversation

---
+++ 07-arrays.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+MOVIES=("pushpa, RRR, Devara")
+# Index starts from 0,. Size is 3
+
+echo "First movie is: ${MOVIES[0]}"
+echo "second movie is: ${MOVIES[1]}"
+echo "Third movie is: ${MOVIES[2]}"
+
+echo "All movies are: ${MOVIES[@]}"
