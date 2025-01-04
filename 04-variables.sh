ae6734f2a934f02710e7e9bd69361a8ef174d9f5
teja-ktvr <itsmektvr@gmail.com>
2025-01-04 19:02:45 +0530

    added conversation

---
+++ 04-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#passing aruguments as values to variables
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Hi $PERSON2"
+echo "$PERSON2:: Hello $PERSON1"
+echo "$PERSON1:: How are you"
+echo "$PERSON2:: I am good. How are you?"
